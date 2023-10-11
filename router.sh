54894bf30f3d1e6740916288cc636a940f25308a
Fawaz <106278241+Kill0geR@users.noreply.github.com>
2023-10-11 08:55:45 +0200

    Update router.sh

--- router.sh
+++ router.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "THANK YOU FOR USING PYTHEAS22"
 echo '"router.sh" WILL BE EXECUTED IN 2 SECONDS'
+spoof = $(sudo apt install dsniff)
 sleep 2
 arp = $(arpspoof -i wlan0 -t router_ip target_ip)
