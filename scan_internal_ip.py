e4779522f021172bea65d3e860a9eab2e73f49f6
Fawaz <106278241+Kill0geR@users.noreply.github.com>
2024-12-02 10:48:10 +0100

    Delete scan_internal_ip.py

--- scan_internal_ip.py
+++
@@ -1,8 +0,0 @@
-from Pytheas22 import Python_Port_Scanner
-
-scanner = Python_Port_Scanner.PythonPortScannerList(well_known_ports=True)
-scanner_lst = scanner.make_lst()
-
-start_scan = Python_Port_Scanner.PythonPortScanner(scanner_lst)
-Python_Port_Scanner.PortScanner.scan_all = True
-start_scan.scan_internal_network()
