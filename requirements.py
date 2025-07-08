6260c9c6931e372978d444f82f8656fc2d43e6fb
Fawaz <106278241+Kill0geR@users.noreply.github.com>
2025-07-08 17:45:00 +0200

    Update requirements.py
    stupid error removed

--- requirements.py
+++ requirements.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import time
-from Pytheas22 import gui_pytheas
 
 
 def install():
@@ -9,18 +8,19 @@ def install():
     time.sleep(1)
     print("pip install Pytheas22")
     os.system("pip install Pytheas22")
-    print("sudo apt install netdiscover")
-    os.system("sudo apt install netdiscover")
-    print("sudo apt install dnsutils")
-    os.system("sudo apt install dnsutils")
-    print("sudo apt install dsniff")
-    os.system("sudo apt install dsniff")
-    print("\n\nFinished\n\n")
+
+    if sys.platform == "linux":
+        print("sudo apt install netdiscover")
+        os.system("sudo apt install netdiscover")
+        print("sudo apt install dnsutils")
+        os.system("sudo apt install dnsutils")
+        print("sudo apt install dsniff")
+        os.system("sudo apt install dsniff")
+        print("\n\nFinished\n\n")
 
 
 if sys.platform == "linux":
     if os.getuid() != 0:
-        print(gui_pytheas)
         print("\nThis program must be run in root!!!!\n".upper())
         quit()
 
